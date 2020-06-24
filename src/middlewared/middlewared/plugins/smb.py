9fcf518c75567c31093c2eb2a189888739871bad
Waqar Ahmed <waqarahmedjoyia@live.com>
2020-06-24 06:33:24 +0500

    Introduce common method to generate alert for locked shares/tasks

--- src/middlewared/middlewared/plugins/smb.py
+++ src/middlewared/middlewared/plugins/smb.py
@@ -582,6 +582,8 @@ class SharingSMBModel(sa.Model):
 
 class SharingSMBService(SharingService):
 
+    share_task_type = 'SMB'
+
     class Config:
         namespace = 'sharing.smb'
         datastore = 'sharing.cifs_share'
